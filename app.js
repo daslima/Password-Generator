aad48e246ddd4522c0caf526d22e6dfc07859ee1
David <davidlimait@outlook.com>
2022-07-08 00:12:00 -0300

    Created the layout and styled the page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const port = 3000
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 // Set Views
 app.set('views', './views')
