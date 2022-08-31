cc1f18e39bf4a2ad021ceb501c825274486bf626
David <davidlimait@outlook.com>
2022-08-30 21:23:46 -0300

    Refactored the path of the files to run the automation

---
+++ cypress/e2e/generatePassword.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('index.html')
+  })
+
+  it('Generate numbers only', () => {
+    
+  });
+
+})
